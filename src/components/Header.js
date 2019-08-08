6deefeed0cc31a8200c1380cadca64077d08a4da
chikwado <chikwadonworie@gamil.com>
2019-08-08 16:00:12 +0100

    add some modification on header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../assets/images/logo.jpg';
 
 class Header extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      activeStyle: true
-    }
-  }
-  render() {
-    return (
+    render() {
+        return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">  
             <div className="container">
               <NavLink to='/' className="navbar-brand"> <img className="navbar-logo" src={logo} alt="logo"/> </NavLink>
@@ -23,26 +17,25 @@ class Header extends Component {
               <span className="navbar-text">
               <ul className="navbar-nav m-2">
                 <li className="nav-item">
-                  
-                  <NavLink style={{color:'black',fontWeight:'bolder'}} to="/" className="nav-link" >Home</NavLink>
+                  <Link style={{color:'black',fontWeight:'bolder'}} to="/" className="nav-link" >Home</Link>
                 </li>
                 <li className="nav-item">
-                  <NavLink activeClassName="activeRoute" activeStyle={{ color: 'red' }} style={{color:'black',fontWeight:'bolder'}} to="/about" className="nav-link" >About</NavLink>
+                  <NavLink activeClassName="activeRoute" style={{color:'black',fontWeight:'bolder'}} to="/about" className="nav-link" >About</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink activeClassName="activeRoute" activeStyle={{ color: 'red' }} style={{color:'black',fontWeight:'bolder'}} to="/speakers" className="nav-link" >Speakers</NavLink>
+                  <NavLink activeClassName="activeRoute" style={{color:'black',fontWeight:'bolder'}} to="/speakers" className="nav-link" >Speakers</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink activeClassName="activeRoute" activeStyle={{ color: 'red' }} style={{color:'black',fontWeight:'bolder'}} to="/sponsors" className="nav-link" >Sponsors</NavLink>
+                  <NavLink activeClassName="activeRoute" style={{color:'black',fontWeight:'bolder'}} to="/sponsors" className="nav-link" >Sponsors</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink activeClassName="activeRoute" activeStyle={{ color: 'red' }} style={{color:'black',fontWeight:'bolder'}} to="/team" className="nav-link" >Team</NavLink>
+                  <NavLink activeClassName="activeRoute" style={{color:'black',fontWeight:'bolder'}} to="/team" className="nav-link" >Team</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink activeClassName="activeRoute" activeStyle={{ color: 'red' }} style={{color:'black',fontWeight:'bolder'}} to="/contact" className="nav-link" >Contact</NavLink>
+                  <NavLink activeClassName="activeRoute" style={{color:'black',fontWeight:'bolder'}} to="/contact" className="nav-link" >Contact</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink  style={{color:'white',fontWeight:'bolder'}} to="/tickets" className="nav-link ticket" >Tickets </NavLink>
+                  <NavLink style={{color:'white',fontWeight:'bolder'}} to="/tickets" className="nav-link ticket" >Tickets </NavLink>
                 </li>
               </ul>
               </span>
@@ -50,7 +43,8 @@ class Header extends Component {
            
             </div>
           </nav>
-    )
-  }
+        );
+    }    
 }
+
 export default Header;
